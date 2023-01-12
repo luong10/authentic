45815ec1393be269663c086428640a8bfc348074
Nguyen Hieu <nguuyenminhhieu28801@gmail.com>
2023-01-12 11:22:07 +0700

    lan1

---
+++ src/Component/Login/login.js
@@ -0,0 +1,96 @@
+import { Button, Checkbox, Form, Input, Card } from "antd";
+import React from "react";
+function Login() {
+  const onFinish = (values) => {
+    console.log("Success:", values);
+  };
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
+  return (
+    <>
+      <div className="site-card-border-less-wrapper">
+        <Card
+          title="Đăng Nhập"
+          bordered={false}
+          style={{
+            width: 800,
+            margin: "0 auto",
+            marginTop: "15%",
+            backgroundColor: "#EEEEEE",
+          }}
+        >
+          <Form
+            name="basic"
+            labelCol={{
+              span: 8,
+            }}
+            wrapperCol={{
+              span: 16,
+            }}
+            initialValues={{
+              remember: true,
+            }}
+            style={{
+              width: 600,
+              margin: "0",
+            }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
+          >
+            <Form.Item
+              label="Username"
+              name="username"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your username!",
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+
+            <Form.Item
+              label="Password"
+              name="password"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your password!",
+                },
+              ]}
+            >
+              <Input.Password />
+            </Form.Item>
+
+            <Form.Item
+              name="remember"
+              valuePropName="checked"
+              wrapperCol={{
+                offset: 8,
+                span: 16,
+              }}
+            >
+              <Checkbox>Remember me</Checkbox>
+            </Form.Item>
+
+            <Form.Item
+              wrapperCol={{
+                offset: 8,
+                span: 16,
+              }}
+            >
+              <Button type="primary" htmlType="submit">
+                Submit
+              </Button>
+            </Form.Item>
+          </Form>
+        </Card>
+      </div>
+    </>
+  );
+}
+
+export default Login;
